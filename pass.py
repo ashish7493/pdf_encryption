e34ec06c2a652b44ae3ef132a6354ed16c9278fb
ashish7493 <102125650+ashish7493@users.noreply.github.com>
2024-08-26 19:57:15 +0530

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,7 +1,7 @@
 import pikepdf
 
-old_pdf=pikepdf.Pdf.open("Ashish Resume_09-08.pdf")
+old_pdf=pikepdf.Pdf.open("Ashish Resume_09-08.pdf") //write the path of the file which you want to encrypt
 
 no_extr=pikepdf.Permissions(extract=False)
 
-old_pdf.save("pass_encry.pdf", encryption=pikepdf.Encryption(user="123asd",owner="ashish",allow=no_extr))
+old_pdf.save("pass_encry.pdf", encryption=pikepdf.Encryption(user="123asd",owner="ashish",allow=no_extr)) //write new pdf name
